159406bd34456069439849b3ec95c5687767e791
Elena Gubarenko <lenagritsishina@gmail.com>
2021-05-16 14:28:00 +0300

    adding changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,10 @@ import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import contactsReducer from './reducers/PhoneBookReducer';
 
-const rootReducer = combineReducers({
-  contactsReducer: contactsReducer,
-});
+// const rootReducer = combineReducers({
+//   contactsReducer: contactsReducer,
+// });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(contactsReducer, composeWithDevTools());
 
 export default store;
